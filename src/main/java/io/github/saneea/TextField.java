365cea6ef2c5cf865d4b4c76644936054c42b591
Alexander Sokol <saneea@bk.ru>
2018-03-04 16:56:00 +0300

    notification about any text changes

--- src/main/java/io/github/saneea/TextField.java
+++ src/main/java/io/github/saneea/TextField.java
@@ -2,6 +2,8 @@ package io.github.saneea;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 import javax.swing.JTextField;
@@ -17,44 +19,50 @@ public class TextField extends JTextField {
 
 	private TextFunction textConverter;
 
+	private final List<Consumer<String>> listeners = new ArrayList<>();
+
+	private String currentValue;
+
 	public TextField(int columns) {
 		super(columns);
-	}
 
-	public void addTextChangeListener(Consumer<String> listener) {
 		addKeyListener(new KeyListener() {
-
-			private String currentValue;
-
-			private void onKey() {
-				String newValue = getText();
-
-				if (newValue != null && !newValue.equals(currentValue)) {
-					currentValue = newValue;
-					listener.accept(currentValue);
-				}
-			}
 
 			@Override
 			public void keyTyped(KeyEvent e) {
-				onKey();
+				onTextChanged();
 			}
 
 			@Override
 			public void keyReleased(KeyEvent e) {
-				onKey();
+				onTextChanged();
 			}
 
 			@Override
 			public void keyPressed(KeyEvent e) {
-				onKey();
+				onTextChanged();
 			}
 		});
+	}
+
+	private void onTextChanged() {
+		String newValue = getText();
+		if (newValue != null && !newValue.equals(currentValue)) {
+			currentValue = newValue;
+			for (Consumer<String> listener : listeners) {
+				listener.accept(newValue);
+			}
+		}
+	}
+
+	public void addTextChangeListener(Consumer<String> listener) {
+		listeners.add(listener);
 	}
 
 	@Override
 	public void setText(String text) {
 		super.setText(prepareText(text));
+		onTextChanged();
 	}
 
 	private String prepareText(String text) {
