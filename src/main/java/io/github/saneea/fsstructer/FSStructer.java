4c3b2088b5c872e3d8e01ef20ef61497d02f64bd
Alexander Sokol <saneea@bk.ru>
2018-04-06 11:25:12 +0300

    draft

--- src/main/java/io/github/saneea/fsstructer/FSStructer.java
+++ src/main/java/io/github/saneea/fsstructer/FSStructer.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
@@ -14,12 +15,69 @@ public class FSStructer {
 	private final Path pathToBadStructure;
 	private final Path pathToGoodStructure;
 
+	private static class MovementProposition {
+		private final Path orig;
+
+		private final List<Path> proposition;
+
+		public MovementProposition(Path orig, List<Path> proposition) {
+			this.orig = orig;
+			this.proposition = proposition;
+		}
+
+		public Path getOrig() {
+			return orig;
+		}
+
+		public List<Path> getProposition() {
+			return proposition;
+		}
+
+	}
+	
+	private static class Movement
+
 	public FSStructer(Path pathToBadStructure, Path pathToGoodStructure) {
 		this.pathToBadStructure = pathToBadStructure;
 		this.pathToGoodStructure = pathToGoodStructure;
 	}
 
+	private List<Path> getInputPaths() throws IOException {
+		return Files.walk(pathToBadStructure).collect(Collectors.toList());
+	}
+
 	public void execute() throws IOException {
+		List<Path> inputPaths = getInputPaths();
+
+		List<MovementProposition> propositions = getMovementPropositions(inputPaths);
+
+		for (MovementProposition proposition : propositions) {
+			processPath(proposition);
+		}
+	}
+
+	private List<MovementProposition> getMovementPropositions(List<Path> inputPaths) throws IOException {
+		List<MovementProposition> propositions = new ArrayList<>();
+		for (Path inputPath : inputPaths) {
+			propositions.add(new MovementProposition(//
+					inputPath, //
+					findFilesByName(inputPath.getFileName().toString())));
+		}
+		return propositions;
+	}
+
+	private List<Path> findFilesByName(String fileName) throws IOException {
+		return Files.find(pathToGoodStructure, Integer.MAX_VALUE, //
+				(path, attributes) -> path.getFileName().toString().equals(fileName))//
+				.collect(Collectors.toList());
+	}
+
+	private void processPath(MovementProposition proposition) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void execute2() throws IOException {
 		Files.walk(pathToBadStructure)//
 				.map(filePath -> {
 
@@ -59,8 +117,13 @@ public class FSStructer {
 			this.newPath = newPath;
 		}
 
-		public void move() throws IOException {
-			Files.move(origPath, newPath);
+		public void move() {
+			try {
+				Files.move(origPath, newPath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 	}
