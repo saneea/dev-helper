aace97e9fd24f95d7878d52b553925343d4aa758
Alexander Sokol <saneea@bk.ru>
2020-08-29 20:26:24 +0300

    small tests refactoring

--- src/test/java/io/github/saneea/feature/XmlReformTest.java
+++ src/test/java/io/github/saneea/feature/XmlReformTest.java
@@ -4,17 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,20 +16,19 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
+import io.github.saneea.TestUtils;
+
 @RunWith(value = Parameterized.class)
 public class XmlReformTest {
 
 	@Parameter
 	public String testId;
 
-	private static final Path TEST_CASES_ROOT = Paths.get("src", "test", "resources", "tests", "xml_pretty_print");
+	private static final Path TEST_CASES_ROOT = TestUtils.TESTS_RESOURCES.resolve("xml_pretty_print");
 
 	@Parameters(name = "{0}")
 	public static List<String[]> testIds() {
-		return Stream.of(//
-				TEST_CASES_ROOT.toFile().list())//
-				.map(dir -> new String[] { dir })//
-				.collect(Collectors.toList());
+		return TestUtils.testIds(TEST_CASES_ROOT);
 	}
 
 	@Test
@@ -44,14 +37,9 @@ public class XmlReformTest {
 
 		String actual = prettyPrint(testRootDir.resolve("input.xml").toString());
 
-		String expected = readFile(testRootDir.resolve("expected.xml"));
+		String expected = TestUtils.readFile(testRootDir.resolve("expected.xml"));
 
 		assertEquals(expected, actual);
-	}
-
-	private String readFile(Path filePath) throws IOException {
-		byte[] encoded = Files.readAllBytes(filePath);
-		return new String(encoded, StandardCharsets.UTF_8);
 	}
 
 	private String prettyPrint(String inputFilePath) throws Exception {
