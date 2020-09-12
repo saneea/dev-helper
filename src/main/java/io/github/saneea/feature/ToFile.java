b8bcdb39db016c957d888a23eeaeced7a124102b
Alexander Sokol <saneea@bk.ru>
2020-09-12 22:47:24 +0300

    feature ToFile is reworked to read std_out and std_err simultaneously (in background threads)

--- src/main/java/io/github/saneea/feature/ToFile.java
+++ src/main/java/io/github/saneea/feature/ToFile.java
@@ -3,40 +3,89 @@ package io.github.saneea.feature;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import io.github.saneea.Feature;
 
 public class ToFile implements Feature {
 
+	interface ExitCode {
+		int OK = 0;
+	}
+
+	interface Arg {
+		int FILE_NAME = 0;
+		int COMMAND_LINE = 1;
+	}
+
 	@Override
 	public void run(InputStream input, OutputStream output, OutputStream err, String[] args) throws Exception {
 
-		String outFileName = args[0];
-		String commandLine = args[1];
+		String outFileName = args[Arg.FILE_NAME];
+		String commandLine = args[Arg.COMMAND_LINE];
 
 		Process forkProc = Runtime.getRuntime().exec(commandLine);
 
-		try (ByteArrayOutputStream buffer = new ByteArrayOutputStream(); //
-				InputStream forkProcOut = new BufferedInputStream(forkProc.getInputStream())) {
-
-			forkProcOut.transferTo(buffer);
-
+		try (ByteArrayBuffer stdOutBuffer = getStdOutFromProc(forkProc, err)) {
 			int exitCode = forkProc.waitFor();
 
-			if (exitCode == 0) {
-				try (OutputStream outFileStream = new BufferedOutputStream(new FileOutputStream(outFileName))) {
-					buffer.writeTo(outFileStream);
-				}
+			if (exitCode == ExitCode.OK) {
+				bufferToFile(stdOutBuffer, outFileName);
 			} else {
-				try (InputStream forkProcErr = new BufferedInputStream(forkProc.getErrorStream())) {
-					forkProcErr.transferTo(err);
-				}
 				System.exit(exitCode);
 			}
 		}
+	}
+
+	private void bufferToFile(ByteArrayBuffer buffer, String fileName) throws IOException {
+		try (OutputStream outFileStream = new BufferedOutputStream(new FileOutputStream(fileName))) {
+			buffer.writeTo(outFileStream);
+		}
+	}
+
+	private ByteArrayBuffer getStdOutFromProc(Process forkProc, OutputStream err)
+			throws InterruptedException, ExecutionException, IOException {
+
+		try (InputStream forkProcOut = new BufferedInputStream(forkProc.getInputStream()); //
+				InputStream forkProcErr = new BufferedInputStream(forkProc.getErrorStream())) {
+
+			ExecutorService executorService = Executors.newFixedThreadPool(2);
+			try {
+
+				Future<ByteArrayOutputStream> transferStdOut = executorService.submit(transferToBuffer(forkProcOut));
+				Future<OutputStream> transferStdErr = executorService.submit(transfer(forkProcErr, err));
+
+				waitFuture(transferStdErr);
+				return ByteArrayBuffer.of(transferStdOut.get());
+			} finally {
+				executorService.shutdown();
+			}
+		}
+	}
+
+	private void waitFuture(Future<?> future) throws InterruptedException, ExecutionException {
+		future.get();
+	}
+
+	private static Callable<ByteArrayOutputStream> transferToBuffer(InputStream in) {
+		return transfer(in, new ByteArrayOutputStream());
+	}
+
+	private static <OutputStreamType extends OutputStream> Callable<OutputStreamType> transfer(InputStream in,
+			OutputStreamType out) {
+		return () -> {
+			in.transferTo(out);
+			return out;
+		};
 	}
 
 	@Override
@@ -45,4 +94,21 @@ public class ToFile implements Feature {
 				"This method should not be used. Use run(InputStream, OutputStream, OutputStream, String[])");
 	}
 
+	private interface ByteArrayBuffer extends Closeable {
+		void writeTo(OutputStream out) throws IOException;
+
+		static ByteArrayBuffer of(ByteArrayOutputStream byteArrayOutputStream) {
+			return new ByteArrayBuffer() {
+				@Override
+				public void writeTo(OutputStream out) throws IOException {
+					byteArrayOutputStream.writeTo(out);
+				}
+
+				@Override
+				public void close() throws IOException {
+					byteArrayOutputStream.close();
+				}
+			};
+		}
+	}
 }
