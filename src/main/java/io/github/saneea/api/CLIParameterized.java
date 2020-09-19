b9de216c2b3f37e0caf3d823e3e2f35096441d5c
Alexander Sokol <saneea@bk.ru>
2020-09-19 21:36:51 +0300

    add --outputEncoding option for UUID feature

--- src/main/java/io/github/saneea/api/CLIParameterized.java
+++ src/main/java/io/github/saneea/api/CLIParameterized.java
@@ -1,6 +1,7 @@
 package io.github.saneea.api;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
 public interface CLIParameterized {
@@ -8,4 +9,18 @@ public interface CLIParameterized {
 	Options createOptions();
 
 	void setCommandLine(CommandLine commandLine);
+
+	interface CommonOptions {
+
+		String OUTPUT_ENCODING = "outputEncoding";
+
+		Option OUTPUT_ENCODING_OPTION = Option//
+				.builder("oe")//
+				.longOpt(OUTPUT_ENCODING)//
+				.hasArg(true)//
+				.argName("encoding")//
+				.required(false)//
+				.desc("output encoding")//
+				.build();
+	}
 }
--- src/main/java/io/github/saneea/feature/UUID.java
+++ src/main/java/io/github/saneea/feature/UUID.java
@@ -3,10 +3,16 @@ package io.github.saneea.feature;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+
 import io.github.saneea.Feature;
 import io.github.saneea.FeatureContext;
+import io.github.saneea.api.CLIParameterized;
 
-public class UUID implements Feature {
+public class UUID implements Feature, CLIParameterized {
+
+	private CommandLine commandLine;
 
 	@Override
 	public String getShortDescription() {
@@ -15,9 +21,23 @@ public class UUID implements Feature {
 
 	@Override
 	public void run(FeatureContext context) throws Exception {
-		try (Writer writer = new OutputStreamWriter(context.getOut())) {
+		String outputEncoding = commandLine.getOptionValue(CommonOptions.OUTPUT_ENCODING);
+		try (Writer writer = outputEncoding == null//
+				? new OutputStreamWriter(context.getOut())//
+				: new OutputStreamWriter(context.getOut(), outputEncoding)) {
 			writer.write(java.util.UUID.randomUUID().toString());
 		}
+	}
+
+	@Override
+	public Options createOptions() {
+		return new Options()//
+				.addOption(CommonOptions.OUTPUT_ENCODING_OPTION);
+	}
+
+	@Override
+	public void setCommandLine(CommandLine commandLine) {
+		this.commandLine = commandLine;
 	}
 
 }
