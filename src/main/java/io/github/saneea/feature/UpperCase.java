164e6023470a807ca7bd9b255e53351877e2f818
Alexander Sokol <saneea@bk.ru>
2020-09-29 23:11:21 +0300

    feature UpperCase refactoring (avoid using input stream from FeatureContext)

--- src/main/java/io/github/saneea/feature/UpperCase.java
+++ src/main/java/io/github/saneea/feature/UpperCase.java
@@ -1,6 +1,5 @@
 package io.github.saneea.feature;
 
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
@@ -8,7 +7,9 @@ import java.io.Writer;
 import io.github.saneea.Feature;
 import io.github.saneea.FeatureContext;
 
-public class UpperCase implements Feature {
+public class UpperCase implements Feature, Feature.In.Text.Reader {
+
+	private Reader in;
 
 	@Override
 	public String getShortDescription() {
@@ -17,11 +18,10 @@ public class UpperCase implements Feature {
 
 	@Override
 	public void run(FeatureContext context) throws Exception {
-		try (Reader reader = new InputStreamReader(context.getIn()); //
-				Writer writer = new OutputStreamWriter(context.getOut())) {
+		try (Writer writer = new OutputStreamWriter(context.getOut())) {
 			char[] buf = new char[4096];
 			int wasRead;
-			while ((wasRead = reader.read(buf)) != -1) {
+			while ((wasRead = in.read(buf)) != -1) {
 				convertChars(buf, wasRead);
 				writer.write(buf, 0, wasRead);
 			}
@@ -37,6 +37,11 @@ public class UpperCase implements Feature {
 
 	private char convertChar(char c) {
 		return Character.toUpperCase(c);
+	}
+
+	@Override
+	public void setReader(Reader in) {
+		this.in = in;
 	}
 
 }
