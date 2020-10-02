e4fc80a6888472c247c42209123fada0c5133e74
Alexander Sokol <saneea@bk.ru>
2020-10-02 23:00:26 +0300

    feature XmlPrettyPrint refactoring (using Feature.In.Text.Reader and Feature.Out.Text.Writer)

--- src/main/java/io/github/saneea/feature/XmlPrettyPrint.java
+++ src/main/java/io/github/saneea/feature/XmlPrettyPrint.java
@@ -1,6 +1,6 @@
 package io.github.saneea.feature;
 
-import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.Writer;
 
 import javax.xml.transform.OutputKeys;
@@ -14,7 +14,10 @@ import javax.xml.transform.stream.StreamSource;
 import io.github.saneea.Feature;
 import io.github.saneea.FeatureContext;
 
-public class XmlPrettyPrint implements Feature {
+public class XmlPrettyPrint implements Feature, Feature.In.Text.Reader, Feature.Out.Text.Writer {
+
+	private Reader in;
+	private Writer out;
 
 	@Override
 	public String getShortDescription() {
@@ -30,10 +33,18 @@ public class XmlPrettyPrint implements Feature {
 		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
 		transformer.setOutputProperty(OutputKeys.INDENT, "yes");// pretty-print
 
-		Source source = new StreamSource(context.getIn());
-		try (Writer writer = new OutputStreamWriter(context.getOut())) {
-			Result result = new StreamResult(writer);
-			transformer.transform(source, result);
-		}
+		Source source = new StreamSource(in);
+		Result result = new StreamResult(out);
+		transformer.transform(source, result);
+	}
+
+	@Override
+	public void setIn(Reader in) {
+		this.in = in;
+	}
+
+	@Override
+	public void setOut(Writer out) {
+		this.out = out;
 	}
 }
