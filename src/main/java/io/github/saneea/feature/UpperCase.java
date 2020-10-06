0333eaacee14e3ce29ab552d1edf2dd55c9c9449
Alexander Sokol <saneea@bk.ru>
2020-10-06 23:28:25 +0300

    support non-buffered i/o streams for UpperCase feature

--- src/main/java/io/github/saneea/feature/UpperCase.java
+++ src/main/java/io/github/saneea/feature/UpperCase.java
@@ -3,13 +3,16 @@ package io.github.saneea.feature;
 import java.io.Reader;
 import java.io.Writer;
 
+import org.apache.commons.cli.CommandLine;
+
 import io.github.saneea.Feature;
 import io.github.saneea.FeatureContext;
 
-public class UpperCase implements Feature, Feature.In.Text.Reader, Feature.Out.Text.Writer {
+public class UpperCase implements Feature, Feature.CLI, Feature.In.Text.Reader, Feature.Out.Text.Writer {
 
 	private Reader in;
 	private Writer out;
+	private CommandLine commandLine;
 
 	@Override
 	public String getShortDescription() {
@@ -18,11 +21,19 @@ public class UpperCase implements Feature, Feature.In.Text.Reader, Feature.Out.T
 
 	@Override
 	public void run(FeatureContext context) throws Exception {
-		char[] buf = new char[4096];
-		int wasRead;
-		while ((wasRead = in.read(buf)) != -1) {
-			convertChars(buf, wasRead);
-			out.write(buf, 0, wasRead);
+		if (commandLine.hasOption(CommonOptions.NON_BUFFERED_STREAMS)) {
+			int codePoint;
+			while ((codePoint = in.read()) != -1) {
+				out.write(Character.toUpperCase(codePoint));
+				out.flush();
+			}
+		} else {
+			char[] buf = new char[4096];
+			int wasRead;
+			while ((wasRead = in.read(buf)) != -1) {
+				convertChars(buf, wasRead);
+				out.write(buf, 0, wasRead);
+			}
 		}
 	}
 
@@ -44,6 +55,11 @@ public class UpperCase implements Feature, Feature.In.Text.Reader, Feature.Out.T
 	@Override
 	public void setOut(Writer out) {
 		this.out = out;
+	}
+
+	@Override
+	public void setCommandLine(CommandLine commandLine) {
+		this.commandLine = commandLine;
 	}
 
 }
