f8f5e02096cbbc9894f49e506bcca2c22f2e8c78
Alexander Sokol <saneea@bk.ru>
2020-10-26 23:49:52 +0300

    Split feature: extract line limit to cli param

--- src/main/java/io/github/saneea/feature/text/Split.java
+++ src/main/java/io/github/saneea/feature/text/Split.java
@@ -4,18 +4,24 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.io.Reader;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+
 import io.github.saneea.Feature;
 import io.github.saneea.FeatureContext;
 
 public class Split implements//
 		Feature, //
+		Feature.CLI, //
 		Feature.In.Text.Reader, //
 		Feature.Out.Text.PrintStream {
 
-	private static final int LINE_SIZE = 60;// TODO set it as CLI param
+	private static final int SIZE_OPT_DEFAULT = 60;
+	private static final String SIZE_OPT = "size";
 
 	private Reader in;
 	private PrintStream out;
+	private CommandLine commandLine;
 
 	@Override
 	public String getShortDescription() {
@@ -24,13 +30,14 @@ public class Split implements//
 
 	@Override
 	public void run(FeatureContext context) throws Exception {
-		while (transferLine()) {
+		long size = getLineSize();
+		while (transferLine(size)) {
 			// no code
 		}
 	}
 
-	private boolean transferLine() throws IOException {
-		for (int i = 0; i < LINE_SIZE; ++i) {
+	private boolean transferLine(long size) throws IOException {
+		for (long i = 0; i < size; ++i) {
 			int charCode = in.read();
 
 			if (charCode == -1) {
@@ -47,6 +54,22 @@ public class Split implements//
 		return true;
 	}
 
+	private long getLineSize() {
+		String sizeStr = commandLine.getOptionValue(SIZE_OPT);
+
+		long size;
+		if (sizeStr == null) {
+			size = SIZE_OPT_DEFAULT;
+		} else {
+			size = Long.parseLong(sizeStr);
+			if (size <= 0) {
+				throw new IllegalArgumentException("Parameter " + SIZE_OPT + " must be greater than 0");
+			}
+		}
+
+		return size;
+	}
+
 	@Override
 	public void setIn(Reader in) {
 		this.in = in;
@@ -56,4 +79,25 @@ public class Split implements//
 	public void setOut(PrintStream out) {
 		this.out = out;
 	}
+
+	@Override
+	public void setCommandLine(CommandLine commandLine) {
+		this.commandLine = commandLine;
+	}
+
+	@Override
+	public Option[] getOptions() {
+		Option[] options = { //
+				Option//
+						.builder("s")//
+						.longOpt(SIZE_OPT)//
+						.hasArg(true)//
+						.argName("line size")//
+						.required(false)//
+						.desc("size limit for each line (default: " + SIZE_OPT_DEFAULT + ")")//
+						.build()//
+		};
+		return options;
+	}
+
 }
