6156021e8b1ab2a569dfffea242a8513cdb9ca25
Alexander Sokol <saneea@bk.ru>
2020-11-28 19:53:01 +0300

    XML feature aliases refactoring

--- src/main/java/io/github/saneea/feature/xml/Xml.java
+++ src/main/java/io/github/saneea/feature/xml/Xml.java
@@ -6,6 +6,7 @@ import java.util.function.Supplier;
 import io.github.saneea.Feature;
 import io.github.saneea.FeatureContext;
 import io.github.saneea.feature.multi.MultiFeatureBase;
+import io.github.saneea.utils.Const;
 
 public class Xml extends MultiFeatureBase {
 
@@ -17,9 +18,13 @@ public class Xml extends MultiFeatureBase {
 	@Override
 	public Map<String, Supplier<Feature>> getFeatureAliases() {
 		return new AliasesBuilder()//
-				.feature("toLine", XmlToLine::new)//
-				.feature("prettyPrint", XmlPrettyPrint::new)//
-				.feature("reform", XmlReform::new)//
+				.multiFeature(Const.FORMAT, //
+						"pretty print / line", //
+						new AliasesBuilder()//
+								.feature(Const.PRETTY, XmlPrettyPrint::new)//
+								.feature(Const.LINE, XmlToLine::new)//
+								.feature("reform", XmlReform::new)//
+								.build())//
 				.build();
 	}
 
