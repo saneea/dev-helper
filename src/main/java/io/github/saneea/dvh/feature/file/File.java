e7a6aa2ee4a5ab0303a3fdee716f3a967fd555cd
Alexander Sokol <saneea@bk.ru>
2021-01-30 00:19:12 +0300

    add File-utils features

---
+++ src/main/java/io/github/saneea/dvh/feature/file/File.java
@@ -0,0 +1,25 @@
+package io.github.saneea.dvh.feature.file;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+import io.github.saneea.dvh.Feature;
+import io.github.saneea.dvh.FeatureContext;
+import io.github.saneea.dvh.feature.multi.MultiFeatureBase;
+
+public class File extends MultiFeatureBase {
+
+	@Override
+	public Meta meta(FeatureContext context) {
+		return Meta.from("file utils");
+	}
+
+	@Override
+	public Map<String, Supplier<Feature>> getFeatureAliases() {
+		return new AliasesBuilder()//
+				.feature("read", ReadFile::new)//
+				.feature("write", WriteFile::new)//
+				.build();
+	}
+
+}
