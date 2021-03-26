55d6b56c10a9cd9e52063d23a138d7b1efbc484c
Alexander Sokol <saneea@bk.ru>
2021-03-27 00:09:54 +0300

    migrate FromHex feature to Kotlin

--- src/main/kotlin/io/github/saneea/dvh/feature/binary/hex/FromHex.kt
+++ src/main/kotlin/io/github/saneea/dvh/feature/binary/hex/FromHex.kt
@@ -1,74 +1,60 @@
-package io.github.saneea.dvh.feature.binary.hex;
+package io.github.saneea.dvh.feature.binary.hex
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
+import io.github.saneea.dvh.Feature
+import io.github.saneea.dvh.Feature.Meta
+import io.github.saneea.dvh.FeatureContext
+import java.io.OutputStream
+import java.io.Reader
+import java.util.*
 
-import io.github.saneea.dvh.Feature;
-import io.github.saneea.dvh.FeatureContext;
+private const val HEX_DIGITS_IN_BYTE = 2
 
-public class FromHex implements Feature, Feature.In.Text.Reader, Feature.Out.Bin.Stream {
+class FromHex :
+    Feature,
+    Feature.In.Text.Reader,
+    Feature.Out.Bin.Stream {
 
-	private static final int HEX_DIGITS_IN_BYTE = 2;
+    private lateinit var reader: Reader
+    private lateinit var output: OutputStream
 
-	private Reader reader;
+    override fun meta(context: FeatureContext) =
+        Meta.from("convert input hex sequence to binary")!!
 
-	private OutputStream output;
 
-	@Override
-	public Meta meta(FeatureContext context) {
-		return Meta.from("convert input hex sequence to binary");
-	}
+    override fun run(context: FeatureContext) {
+        while (true) {
+            val buf = readCharsFromStream(reader)
+            if (buf.size < HEX_DIGITS_IN_BYTE) {
+                break
+            }
 
-	@Override
-	public void run(FeatureContext context) throws IOException {
-		while (true) {
+            val digits = buf.map { Character.digit(it, 16) }
+            val byteCode = (digits[0] shl 4) + digits[1]
 
-			List<Character> buf = readCharsFromStream(reader);
+            output.write(byteCode)
+        }
+    }
 
-			if (buf == null || buf.size() < HEX_DIGITS_IN_BYTE) {
-				break;
-			}
+    private fun readCharsFromStream(reader: Reader): List<Char> {
+        val outChars: MutableList<Char> = ArrayList(HEX_DIGITS_IN_BYTE)
+        val tempBuf = CharArray(HEX_DIGITS_IN_BYTE)
+        while (outChars.size < HEX_DIGITS_IN_BYTE) {
+            val len = reader.read(tempBuf)
+            if (len == -1) {
+                break
+            }
+            for (i in 0 until len) {
+                outChars.add(tempBuf[i])
+            }
+        }
+        return outChars
+    }
 
-			int digit0 = Character.digit(buf.get(0), 16);
-			int digit1 = Character.digit(buf.get(1), 16);
+    override fun setIn(reader: Reader) {
+        this.reader = reader
+    }
 
-			int byteCode = ((digit0 << 4) + digit1);
-
-			output.write(byteCode);
-		}
-	}
-
-	private List<Character> readCharsFromStream(Reader reader) throws IOException {
-
-		List<Character> outChars = new ArrayList<>(HEX_DIGITS_IN_BYTE);
-
-		char[] tempBuf = new char[HEX_DIGITS_IN_BYTE];
-
-		while (outChars.size() < HEX_DIGITS_IN_BYTE) {
-			int len = reader.read(tempBuf);
-			if (len == -1) {
-				return null;
-			}
-
-			for (int i = 0; i < len; ++i) {
-				outChars.add(tempBuf[i]);
-			}
-		}
-
-		return outChars;
-	}
-
-	@Override
-	public void setIn(Reader reader) {
-		this.reader = reader;
-	}
-
-	@Override
-	public void setOut(OutputStream output) {
-		this.output = output;
-	}
-
+    override fun setOut(output: OutputStream) {
+        this.output = output
+    }
 }
