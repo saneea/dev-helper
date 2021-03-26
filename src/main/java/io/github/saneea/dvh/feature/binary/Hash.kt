84aa714d7834f35884d093dbcca19737718e13a7
Alexander Sokol <saneea@bk.ru>
2021-03-27 01:03:54 +0300

    migrate Hash feature to Kotlin

--- src/main/java/io/github/saneea/dvh/feature/binary/Hash.kt
+++ src/main/java/io/github/saneea/dvh/feature/binary/Hash.kt
@@ -1,95 +1,77 @@
-package io.github.saneea.dvh.feature.binary;
+package io.github.saneea.dvh.feature.binary
 
-import io.github.saneea.dvh.Feature;
-import io.github.saneea.dvh.FeatureContext;
-import io.github.saneea.dvh.feature.binary.hex.ToHex;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
+import io.github.saneea.dvh.Feature
+import io.github.saneea.dvh.Feature.CLI
+import io.github.saneea.dvh.Feature.Meta
+import io.github.saneea.dvh.FeatureContext
+import io.github.saneea.dvh.feature.binary.hex.ToHex
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.Option
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+import java.io.PrintStream
+import java.security.MessageDigest
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+class Hash :
+    Feature,
+    CLI,
+    CLI.Options,
+    Feature.In.Bin.Stream,
+    Feature.Out.Text.PrintStream {
 
-public class Hash implements//
-		Feature, //
-		Feature.CLI, //
-		Feature.CLI.Options, //
-		Feature.In.Bin.Stream, //
-		Feature.Out.Text.PrintStream {
+    private lateinit var commandLine: CommandLine
+    private lateinit var `in`: InputStream
+    private lateinit var out: PrintStream
 
-	private static int BUFFER_SIZE = 4096;
+    override fun meta(context: FeatureContext) =
+        Meta.from("calc hash (md5, sha-* etc.)")!!
 
-	private CommandLine commandLine;
+    override fun run(context: FeatureContext) {
+        val alg = commandLine.getOptionValue(Params.ALGORITHM)
+        execute(`in`, out, alg)
+    }
 
-	private InputStream in;
-	private PrintStream out;
+    override fun getOptions() = Params.createOptions()
 
-	@Override
-	public Meta meta(FeatureContext context) {
-		return Meta.from("calc hash (md5, sha-* etc.)");
-	}
+    override fun setCommandLine(commandLine: CommandLine) {
+        this.commandLine = commandLine
+    }
 
-	private static void execute(InputStream input, PrintStream out, String alg)
-			throws NoSuchAlgorithmException, IOException {
+    object Params {
+        const val ALGORITHM = "algorithm"
+        fun createOptions(): Array<Option> {
+            return arrayOf( //
+                Option //
+                    .builder("a") //
+                    .longOpt(ALGORITHM) //
+                    .hasArg(true) //
+                    .argName("algorithm name") //
+                    .required(true) //
+                    .desc("hash algorithm (e.g. md5)") //
+                    .build()
+            )
+        }
+    }
 
-		MessageDigest md = MessageDigest.getInstance(alg);
+    override fun setOut(out: PrintStream) {
+        this.out = out
+    }
 
-		byte[] buf = new byte[BUFFER_SIZE];
+    override fun setIn(`in`: InputStream) {
+        this.`in` = `in`
+    }
 
-		int len;
-		while ((len = input.read(buf)) != -1) {
-			md.update(buf, 0, len);
-		}
+    companion object {
+        private const val BUFFER_SIZE = 4096
 
-		ToHex.Companion.run(new ByteArrayInputStream(md.digest()), out);
-	}
-
-	@Override
-	public void run(FeatureContext context) throws NoSuchAlgorithmException, IOException {
-		String alg = commandLine.getOptionValue(Params.ALGORITHM);
-		execute(in, out, alg);
-	}
-
-	@Override
-	public Option[] getOptions() {
-		return Params.createOptions();
-	}
-
-	@Override
-	public void setCommandLine(CommandLine commandLine) {
-		this.commandLine = commandLine;
-	}
-
-	public static class Params {
-
-		public static final String ALGORITHM = "algorithm";
-
-		private static Option[] createOptions() {
-
-			Option[] options = { //
-					Option//
-							.builder("a")//
-							.longOpt(ALGORITHM)//
-							.hasArg(true)//
-							.argName("algorithm name")//
-							.required(true)//
-							.desc("hash algorithm (e.g. md5)")//
-							.build() };
-
-			return options;
-		}
-	}
-
-	@Override
-	public void setOut(PrintStream out) {
-		this.out = out;
-	}
-
-	@Override
-	public void setIn(InputStream in) {
-		this.in = in;
-	}
+        private fun execute(input: InputStream, out: PrintStream, alg: String) {
+            val md = MessageDigest.getInstance(alg)
+            val buf = ByteArray(BUFFER_SIZE)
+            var len: Int
+            while (input.read(buf).also { len = it } != -1) {
+                md.update(buf, 0, len)
+            }
+            ToHex.run(ByteArrayInputStream(md.digest()), out)
+        }
+    }
 }
