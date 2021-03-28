a71ed1cb17d572baf63a6d3e1c754b43e51b9cf7
Alexander Sokol <saneea@bk.ru>
2021-03-28 21:44:13 +0300

    migrate Json features to Kotlin

---
+++ src/main/java/io/github/saneea/dvh/feature/json/Json.kt
@@ -0,0 +1,26 @@
+package io.github.saneea.dvh.feature.json
+
+import io.github.saneea.dvh.Feature
+import io.github.saneea.dvh.Feature.Meta
+import io.github.saneea.dvh.FeatureContext
+import io.github.saneea.dvh.feature.multi.MultiFeatureBase
+import io.github.saneea.dvh.utils.Const
+import java.util.function.Supplier
+
+class Json : MultiFeatureBase() {
+
+    override fun meta(context: FeatureContext) =
+        Meta.from("json processing")!!
+
+    override fun getFeatureAliases(): MutableMap<String, Supplier<Feature>> =
+        AliasesBuilder()
+            .multiFeature(
+                Const.FORMAT,
+                "pretty print / line",
+                AliasesBuilder()
+                    .feature(Const.PRETTY, JsonFormatting::Pretty)
+                    .feature(Const.LINE, JsonFormatting::Line)
+                    .build()
+            )
+            .build()
+}
---
+++ src/main/java/io/github/saneea/dvh/feature/json/JsonFormatting.kt
@@ -0,0 +1,51 @@
+package io.github.saneea.dvh.feature.json
+
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
+import io.github.saneea.dvh.Feature
+import io.github.saneea.dvh.Feature.Meta
+import io.github.saneea.dvh.FeatureContext
+import java.io.Reader
+import java.io.Writer
+
+class JsonFormatting {
+
+    class Pretty : Base() {
+        override fun meta(context: FeatureContext) =
+            Meta.from("format JSON with indents")!!
+
+        override val gsonBuilder: GsonBuilder = GsonBuilder().setPrettyPrinting()
+    }
+
+    class Line : Base() {
+        override fun meta(context: FeatureContext) =
+            Meta.from("format JSON to line")!!
+
+        override val gsonBuilder = GsonBuilder()
+    }
+
+    abstract class Base :
+        Feature,
+        Feature.In.Text.Reader,
+        Feature.Out.Text.Writer {
+
+        private lateinit var `in`: Reader
+        private lateinit var out: Writer
+
+        protected abstract val gsonBuilder: GsonBuilder
+
+        override fun run(context: FeatureContext) {
+            val gson = gsonBuilder.create()
+            val jsonElement = gson.fromJson(`in`, JsonElement::class.java)
+            gson.toJson(jsonElement, out)
+        }
+
+        override fun setIn(`in`: Reader) {
+            this.`in` = `in`
+        }
+
+        override fun setOut(out: Writer) {
+            this.out = out
+        }
+    }
+}
