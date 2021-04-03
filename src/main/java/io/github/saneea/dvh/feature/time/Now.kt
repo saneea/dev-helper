778f73558d804f6e224bc290679e5413d3fc47c8
Alexander Sokol <saneea@bk.ru>
2021-04-04 01:12:38 +0300

    migrate Time/Now and Sleep features to Kotlin

---
+++ src/main/java/io/github/saneea/dvh/feature/time/Now.kt
@@ -0,0 +1,75 @@
+package io.github.saneea.dvh.feature.time
+
+import io.github.saneea.dvh.Feature
+import io.github.saneea.dvh.Feature.Meta
+import io.github.saneea.dvh.Feature.Util.IOConsumer
+import io.github.saneea.dvh.FeatureContext
+import io.github.saneea.dvh.feature.time.format.FormatFactory
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.Option
+import java.time.ZonedDateTime
+
+class Now :
+    Feature,
+    Feature.CLI,
+    Feature.CLI.Options,
+    Feature.Out.Text.String {
+
+    private lateinit var out: IOConsumer<String>
+    private lateinit var commandLine: CommandLine
+    private val formatFactory = FormatFactory()
+
+    override fun meta(context: FeatureContext): Meta {
+        val featuresChain = context.featuresChainString
+        return Meta.from(
+            Meta.Description.from(
+                "print current time",
+                "print current date and/or time in one of format"
+            ),
+            listOf(
+                Meta.Example.from(
+                    "seconds from 1970-01-01 (known as Unix-time or Epoch-time)",
+                    "$featuresChain -$FORMAT_SHORT unix",
+                    "1606339702"
+                ),
+                Meta.Example.from(
+                    "custom date/time pattern",
+                    "$featuresChain -$FORMAT_SHORT yyyy-MM-dd--HH:mm:ss",
+                    "2020-11-26--00:36:55"
+                )
+            )
+        )
+    }
+
+    override fun run(context: FeatureContext) {
+        val formatNameOrPattern = commandLine.getOptionValue(FORMAT, FormatFactory.FORMAT_HUMAN)
+        val format = formatFactory.createFormat(formatNameOrPattern)
+        val formattedTime = format.render(ZonedDateTime.now())
+        out.accept(formattedTime)
+    }
+
+    override fun setOut(out: IOConsumer<String>) {
+        this.out = out
+    }
+
+    override fun setCommandLine(commandLine: CommandLine) {
+        this.commandLine = commandLine
+    }
+
+    override fun getOptions(): Array<Option> =
+        arrayOf(
+            Option
+                .builder(FORMAT_SHORT)
+                .longOpt(FORMAT)
+                .hasArg(true)
+                .argName(formatFactory.availableFormatsString)
+                .required(false)
+                .desc("output time format")
+                .build()
+        )
+
+    companion object {
+        private const val FORMAT = "format"
+        private const val FORMAT_SHORT = "f"
+    }
+}
