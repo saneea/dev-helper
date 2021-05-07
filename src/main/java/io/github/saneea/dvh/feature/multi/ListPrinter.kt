8528a29fa0973622833c63c76ac29afa35b490d9
Alexander Sokol <saneea@bk.ru>
2021-05-07 23:47:21 +0300

    migrate ListPrinter class to Kotlin

---
+++ src/main/java/io/github/saneea/dvh/feature/multi/ListPrinter.kt
@@ -0,0 +1,21 @@
+package io.github.saneea.dvh.feature.multi
+
+import io.github.saneea.dvh.FeatureContext
+import io.github.saneea.dvh.FeatureProvider
+import io.github.saneea.dvh.utils.Utils
+import java.io.PrintStream
+
+class ListPrinter(out: PrintStream?) : FeatureCatalogPrinter(out) {
+
+    public override fun print(featureProvider: FeatureProvider, context: FeatureContext) {
+        val featuresNames = featureProvider.featuresNames()
+        val maxFeatureNameSize = Utils.getMaxStringLength(featuresNames)
+        for (featureName in featuresNames) {
+            val featureInfo = featureProvider.featureInfo(
+                FeatureContext(context, featureName, Utils.NO_ARGS)
+            )
+            val template = "%1$" + maxFeatureNameSize + "s - %2\$s"
+            out.println(String.format(template, featureName, featureInfo.description))
+        }
+    }
+}
