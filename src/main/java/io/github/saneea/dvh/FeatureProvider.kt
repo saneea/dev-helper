b4d797bad0e53da149c7628560d9ff5cfb2b84aa
Alexander Sokol <saneea@bk.ru>
2021-05-08 16:14:30 +0300

    migrate FeatureProvider class to Kotlin

---
+++ src/main/java/io/github/saneea/dvh/FeatureProvider.kt
@@ -0,0 +1,19 @@
+package io.github.saneea.dvh
+
+import io.github.saneea.dvh.feature.multi.FeatureInfo
+
+interface FeatureProvider {
+
+    fun featuresNames(): Set<String>
+
+    fun createFeature(featureName: String): Feature?
+
+    fun featureInfo(context: FeatureContext): FeatureInfo {
+        return try {
+            val feature = createFeature(context.featureName!!)
+            FeatureInfo(feature, feature!!.meta(context).description().brief())
+        } catch (e: Exception) {
+            FeatureInfo(null, e.toString())
+        }
+    }
+}
