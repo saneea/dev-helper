46f9f5329c642f1bab75740757e7ef4c45a704c3
Alexander Sokol <saneea@bk.ru>
2021-05-10 08:15:27 +0300

    migrate String2CppArrayTest class to Kotlin

---
+++ src/test/java/io/github/saneea/dvh/textfunction/String2CppArrayTest.kt
@@ -0,0 +1,32 @@
+package io.github.saneea.dvh.textfunction
+
+import io.github.saneea.dvh.FeatureContext
+import io.github.saneea.dvh.feature.text.ToCharArray
+import org.junit.Assert
+import org.junit.Test
+import java.io.StringReader
+import java.io.StringWriter
+
+class String2CppArrayTest {
+
+    @Test
+    fun testSimple() = test("abc", "'a', 'b', 'c'")
+
+    @Test
+    fun testEscapingQuote() = test("a'bc", "'a', '\\'', 'b', 'c'")
+
+    @Test
+    fun testEscapingBackslash() = test("a\\bc", "'a', '\\\\', 'b', 'c'")
+
+    private fun test(input: String, expected: String) {
+        val toCharArray = ToCharArray()
+        toCharArray.setIn(StringReader(input))
+        val out = StringWriter()
+        toCharArray.setOut(out)
+
+        val featureContext = FeatureContext(null, "any", emptyArray())
+        toCharArray.run(featureContext)
+        val actual = out.toString()
+        Assert.assertEquals(expected, actual)
+    }
+}
