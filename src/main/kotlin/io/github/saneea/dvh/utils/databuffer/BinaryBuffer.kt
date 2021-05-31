fca21198721727cad5bc8720fd782c70bb5daa8e
Alexander Sokol <saneea@bk.ru>
2021-06-01 00:21:39 +0300

    add writeOnClose function

--- src/main/kotlin/io/github/saneea/dvh/utils/databuffer/BinaryBuffer.kt
+++ src/main/kotlin/io/github/saneea/dvh/utils/databuffer/BinaryBuffer.kt
@@ -7,3 +7,41 @@ interface BinaryBuffer : AutoCloseable {
     val outputStream: OutputStream
     val inputStream: InputStream
 }
+
+private const val MiB = 1024 * 1024
+
+fun createDefaultBuffer(): BinaryBuffer =
+    SwitchableBinaryBuffer(
+        ArrayBinaryBuffer(),
+        FileBinaryBuffer(),
+        bufferSize
+    )
+
+fun writeOnClose(
+    createTargetStream: () -> OutputStream,
+    createBuffer: () -> BinaryBuffer = ::createDefaultBuffer
+): OutputStream {
+
+    return object : OutputStream() {
+
+        val buffer = createBuffer()
+
+        override fun write(b: Int) = buffer.outputStream.write(b)
+
+        override fun close() {
+            buffer.use { buffer ->
+                buffer.outputStream.close()
+                createTargetStream().use { targetStream ->
+                    buffer.inputStream.use { bufferInputStream ->
+                        bufferInputStream.transferTo(targetStream)
+                    }
+                }
+            }
+        }
+    }
+}
+
+private val bufferSize
+    get() = System.getProperty("binBufferSize")
+        ?.toLong()
+        ?: 50L * MiB
